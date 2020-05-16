4b373d8e33d4a4974a8cf233e08f3569d2ca1acf
Dr.Shane <ShaneArpas@outlook.com>
2020-05-16 23:10:33 +1000

    added migration assembly to stores

--- IdentityServerMysql/Startup.cs
+++ IdentityServerMysql/Startup.cs
@@ -52,12 +52,12 @@ namespace IdentityServerMysql
                 // this adds the config data from DB (clients, resources, CORS)
                 .AddConfigurationStore(options =>
                 {
-                    options.ConfigureDbContext = builder => builder.UseMySql(connectionString);
+                    options.ConfigureDbContext = builder => builder.UseMySql(connectionString, mysql => mysql.MigrationsAssembly(migrationsAssembly));
                 })
                 // this adds the operational data from DB (codes, tokens, consents)
                 .AddOperationalStore(options =>
                 {
-                    options.ConfigureDbContext = builder => builder.UseMySql(connectionString);
+                    options.ConfigureDbContext = builder => builder.UseMySql(connectionString, mysql => mysql.MigrationsAssembly(migrationsAssembly));
 
                     // this enables automatic token cleanup. this is optional.
                     options.EnableTokenCleanup = true;
