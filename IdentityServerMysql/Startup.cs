a4dd07b96142b2e6bc42f4e13326deca74ed3f40
Dr.Shane <ShaneArpas@outlook.com>
2020-05-16 22:05:25 +1000

    added using efcoremigrations

--- IdentityServerMysql/Startup.cs
+++ IdentityServerMysql/Startup.cs
@@ -5,6 +5,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
