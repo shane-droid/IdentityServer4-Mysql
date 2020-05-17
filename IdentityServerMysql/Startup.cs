d7b807a538068af6696da8bbfe5fb0d90e124c5e
Dr.Shane <ShaneArpas@outlook.com>
2020-05-17 12:03:20 +1000

    add iniitalise database in startupcs

--- IdentityServerMysql/Startup.cs
+++ IdentityServerMysql/Startup.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace IdentityServerMysql
@@ -68,6 +71,7 @@ namespace IdentityServerMysql
 
         }
 
+  
         public void Configure(IApplicationBuilder app)
         {
             if (Environment.IsDevelopment())
@@ -87,6 +91,54 @@ namespace IdentityServerMysql
             //{
             //    endpoints.MapDefaultControllerRoute();
             //});
+        }
+
+        /// <summary>
+        ///  help initialize the database
+        ///  
+        /// 
+        /// add this line: InitializeDatabase(app);  in public void Configure(IApplicationBuilder app)
+        ///  
+        /// InitializeDatabase helper API is convenient to seed the database, 
+        /// but this approach is not ideal to leave in to execute each time the applicaion runs. 
+        /// Once your database is populated, consider removing the call to the API.
+        /// </summary>
+        /// <param name="app"></param>
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
