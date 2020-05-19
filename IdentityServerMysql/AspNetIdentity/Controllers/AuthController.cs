7d8fa7c06aca3f5e0da13f0e06ba69bd3e11d024
Dr.Shane <ShaneArpas@outlook.com>
2020-05-19 18:34:56 +1000

    created custom Login: Authcontroller Login LoginViewModel ViewImports

---
+++ IdentityServerMysql/AspNetIdentity/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServerMysql.AspNetIdentity.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string returnUrl) //collects the incomming Url from the customer
+        {
+            return View(new LoginViewModelASpid { ReturnUrl = returnUrl });// takes the url, passes it to the model, 
+                                                                            //and then passes the model to the view
+                                                                            //and returns the view to customer
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModelASpid Vm)
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityServerMysql/AspNetIdentity/Controllers/LoginViewModelASpid.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityServerMysql.AspNetIdentity.Controllers
+{
+    public class LoginViewModelASpid
+    {
+        // for aspid auth controller
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
